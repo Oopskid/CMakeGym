41c02d71e21e41a0c153b3c93856ba7e24548cd6
Adam Hughes <ahughesformal@gmail.com>
2022-08-06 22:06:51 +0100

    Added startup project switch

--- scripts/core.cmake
+++ scripts/core.cmake
@@ -1,7 +1,25 @@
+include(${CMAKE_CURRENT_LIST_DIR}/modules.cmake)
+startScript(gymcore)
 # Core script to manage CMake'd projects
+
+# VS switch
+if(CMAKE_GENERATOR MATCHES "Visual Studio")
+   set(isVS true)
+else()
+   set(isVS false)
+endif()
 
 # Define a dead function
 macro(deadfunc name)
    macro(${name})
    endmacro()
 endmacro()
+
+# Setup a project as the main one
+macro(setMain name)
+   if(${isVS})
+      set(VS_STARTUP_PROJECT ${name})
+   endif()
+endmacro()
+
+endScript(gymcore)
