e781b73d0c7b7aec6eae1fc0aa75fb448cef9304
Adam Hughes <ahughesformal@gmail.com>
2022-08-06 22:02:59 +0100

    Made logging variable in argument call and fixed bugs

--- scripts/debug.cmake
+++ scripts/debug.cmake
@@ -3,9 +3,9 @@
 include(scripts/core.cmake)
 
 # CMake debugging
-set(CM_DEBUG CACHE BOOL FALSE)
+set(CM_DEBUG FALSE CACHE BOOL "Sets whether CMake scripts will be debugged")
 # CMake verbose prints
-set(CM_VERBOSE CACHE BOOL FALSE)
+set(CM_VERBOSE FALSE CACHE BOOL "Sets whether verbose logs will be printed")
 # CMake call stack
 set(CM_CALLSTACK "" CACHE INTERNAL STRING)
 # Log
@@ -23,7 +23,7 @@ if(CM_DEBUG)
    endmacro()
 
    #Macro for logging error info (with call stack)
-   macro(logError shorttxt longtxt)
+   macro(logErrorINTERNAL shorttxt longtxt)
       if(CM_VERBOSE)
          message(SEND_ERROR ${CM_CONSOLEPX} ${longtxt} " with trace: \n" ${CM_CALLSTACK})
       else()
@@ -31,12 +31,28 @@ if(CM_DEBUG)
       endif()
    endmacro()
 
+   macro(logError shorttxt #[[longtxt]])
+      if(${ARGC} GREATER 1)
+         logErrorINTERNAL(${shorttxt} ${ARGV1})
+      else()
+         logErrorINTERNAL(${shorttxt} ${shorttxt})
+      endif()
+   endmacro()
+
    # Macro for logging debug info
-   macro(logInf shorttxt longtxt)
+   macro(logInfINTERNAL shorttxt longtxt)
       if(CM_VERBOSE)
          message(STATUS ${CM_CONSOLEPX} ${longtxt})
       else()
          message(STATUS ${CM_CONSOLEPX} ${shorttxt})
+      endif()
+   endmacro()
+
+   macro(logInf shorttxt #[[longtxt]])
+      if(${ARGC} GREATER 1)
+         logInfINTERNAL(${shorttxt} ${ARGV1})
+      else()
+         logInfINTERNAL(${shorttxt} ${shorttxt})
       endif()
    endmacro()
 else()
