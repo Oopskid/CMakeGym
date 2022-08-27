09f82959b2f4937466bb3c37c267716d93db5073
Adam Hughes <ahughesformal@gmail.com>
2022-08-27 23:45:43 +0100

    Complete hint file read

--- scripts/fileops.cmake
+++ scripts/fileops.cmake
@@ -13,7 +13,6 @@ macro(getDirFiles returnList location fileTypes)
    endforeach()
 endmacro()
 
-#[[
 # Returns text within braces
 macro(getBraced text delimfront delimback out)
    set(regexpr "[" ${delimfront} "].*[" ${delimback} "]")
@@ -22,16 +21,18 @@ endmacro()
 
 # Deciphers a component of a hint file into values
 # TODO: This can be optimized
-function(hintchunk text concerns values)
-   string(REGEX REPLACE "[{].*" "" ${concerns} ${text})
-   string(REGEX REPLACE "[ ,\n\r]" "" ${concerns} ${${concerns}})
-   string(REGEX REPLACE "\"\"" " " ${concerns} ${${concerns}})
-   string(REGEX REPLACE "\"" "" ${concerns} ${${concerns}})
-   
-   string(REGEX MATCH "[{].*" ${values} ${text})
-   string(REGEX REPLACE "[}].*" "" ${values} ${${values}})
-   string(REGEX REPLACE "[{}]" "" ${values} ${${values}})
-endfunction()
+macro(hintchunk text concerns values)
+   string(REGEX REPLACE "[{].*" "" "${concerns}" "${text}")
+   string(REGEX REPLACE "[ ,\n\r]" "" "${concerns}" "${${concerns}}")
+   string(REGEX REPLACE "\"\"" ";" "${concerns}" "${${concerns}}")
+   string(REGEX REPLACE "\"" "" "${concerns}" "${${concerns}}")
+
+   string(REGEX MATCH "[{].*" "${values}" "${text}")
+   string(REGEX REPLACE "[}].*" "" "${values}" "${${values}}")
+   string(REGEX REPLACE "[{}]" "" "${values}" "${${values}}")
+   string(REGEX REPLACE "[\n]" ";" "${values}" "${${values}}")
+   string(REGEX REPLACE "[ ,\n\r]" "" "${values}" "${${values}}")
+endmacro()
 
 # Appends values from a hint file
 function(hintdecipher text prefix)
@@ -43,37 +44,28 @@ sfunc(hintdecipher)
       firebreak(out)
 
       # Process next chunk of values
-      logInf(${text} ${text})
-      hintchunk(${text} VARS_CAUGHT VALUES_CAUGHT)
+      hintchunk("${text}" VARS_CAUGHT VALUES_CAUGHT)
       set(END_POS)
-      string(FIND ${text} "}" END_POS)
+      string(FIND "${text}" "}" END_POS)
       if(${END_POS} LESS_EQUAL 0)
          break()
       endif()
       MATH(EXPR END_POS "${END_POS}+1")
       set(LEN_SUB)
-      string(LENGTH ${text} LEN_SUB)
+      string(LENGTH "${text}" LEN_SUB)
       MATH(EXPR LEN_SUB "${END_POS}-${END_POS}-1")
       # Remove progress from text
-      string(SUBSTRING ${text} ${END_POS} ${LEN_SUB} text)
+      string(SUBSTRING "${text}" ${END_POS} ${LEN_SUB} text)
 
       # Append values to found variables
-      foreach(VAR_I IN LISTS ITEMS bb aa ff)
-         if(NOT DEFINED ${prefix}${VAR_I})
-            set(${prefix}${VAR_I} CACHE STRING FORCE)
-            logInf(${prefix}${VAR_I} ${prefix}${VAR_I})
+      foreach(VAR_I ${VARS_CAUGHT})
+         if(NOT DEFINED "${prefix}${VAR_I}")
+            set("${prefix}${VAR_I}" CACHE INTERNAL STRING)
          endif()
-         set(${prefix}${VAR_I} ${VALUES_CAUGHT} CACHE STRING FORCE)
-         #logInf(${${prefix}""${VAR_I}} ${${prefix}""${VAR_I}})
+         set("${prefix}${VAR_I}" "${VALUES_CAUGHT}${${prefix}${VAR_I}}" CACHE INTERNAL STRING)
       endforeach()
    endwhile()
 efunc()
 endfunction()
-
-
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/hints/codefiles.hint test_fiel)
-hintdecipher(${test_fiel} "__")
-#logInf(${__bb} ${__bb})
-]]
 
 endScript(fileOps)
--- scripts/strings.cmake
+++ scripts/strings.cmake
@@ -2,7 +2,7 @@
 
 # Evaluates to whether a string variable logically exists
 macro(strexists str)
-   if(NOT ${str} STREQUAL "")
+   if(NOT "${str}" STREQUAL "")
       set(out 1)
    else()
       set(out 0)
