b22a658d7517aecf876d733a15a1ec5b99cdf351
Adam Hughes <ahughesformal@gmail.com>
2023-02-05 04:10:50 +0000

    Improves HLSL path usage and supports hlsli

--- platform/win/D3D/hlsl.cmake
+++ platform/win/D3D/hlsl.cmake
@@ -18,8 +18,9 @@ set(HLSL_TYPE_COMPUTE "cs" CACHE INTERNAL STRING) # Compute
 set(HLSL_TYPE_HULL "hs" CACHE INTERNAL STRING) # Hull
 set(HLSL_TYPE_DOMAIN "ds" CACHE INTERNAL STRING) # Domain
 set(HLSL_TYPE_GEOMETRY "gs" CACHE INTERNAL STRING) # Geometry
+set(HLSL_TYPE_HEADER "lib" CACHE INTERNAL STRING) # Header
 
-macro(defineShaders target type shaders)
+macro(defineShaders target type shaders #[[strictlyFormatted]])
    # Defaults to global
    set(HLSL_${target}_SHADER_MODEL "${HLSL_SHADER_GLOBAL_MODEL_MAJOR}_${HLSL_SHADER_GLOBAL_MODEL_MINOR}" CACHE STRING "The shader model used for target ${target}")
    set(HLSL_${target}_WORKDIRECTORY "${HLSL_SHADER_GLOBAL_WORK_DIRECTORY}" CACHE STRING "The working directory for target ${target}")
@@ -29,29 +30,39 @@ macro(defineShaders target type shaders)
    # Shorthands
    set(trueOutputDir "$<TARGET_FILE_DIR:${target}>/${HLSL_${target}_OUTPUTDIRECTORY}")
    set(trueCopyDir "$<TARGET_FILE_DIR:${target}>/${HLSL_${target}_COPYDIRECTORY}")
+   set(workDir ${HLSL_${target}_WORKDIRECTORY})
+
+   # Do not compile if strictly formatted (i.e. hlsli)
+   set(compileBatch 1)
+   if(${ARGC} GREATER 3)
+       if(${ARGV3})
+          set(compileBatch 0)
+       endif()
+   endif()
 
    # Compiled shaders
-   foreach(shader "${shaders}")
+   foreach(shader ${shaders})
       getPureFilename(${shader} shaderFilename)
       string(REPLACE "${HLSL_${target}_WORKDIRECTORY}" "" shaderRelFile "${shader}")
       getDir(${shaderRelFile} shaderRel)
-      if(NOT HLSL_${target}_OUTPUTDIRECTORY STREQUAL "")
-         add_custom_command(TARGET ${target}
-            COMMAND ${HLSL_SHADER_GLOBAL_COMPILER} /nologo /E main /T ${type}_${HLSL_${target}_SHADER_MODEL} $<IF:$<CONFIG:DEBUG>,/Od,/O${HLSL_SHADER_GLOBAL_OPTIMIZATION}> /Zi /Fo ${trueOutputDir}/${shaderRel}/${shaderFilename}.cso ${shader}
-            MAIN_DEPENDENCY ${shader}
-            COMMENT "HLSL COMPILE ${shader}"
-            WORKING_DIRECTORY ${HLSL_${target}_WORKDIRECTORY}
-            VERBATIM)
-      endif()
       # Formatted
       if(NOT HLSL_${target}_COPYDIRECTORY STREQUAL "")
          add_custom_command(TARGET ${target}
             POST_BUILD
             COMMAND ${CMAKE_COMMAND} 
-            ARGS -E copy_if_different "${shaders}" "${trueCopyDir}/${shaderRelFile}"
+            ARGS -E copy_if_different "${workDir}/${shaderRelFile}" "${trueCopyDir}/${shaderRelFile}"
             MAIN_DEPENDENCY ${shader}
             COMMENT "HLSL COPY ${shader}"
-            WORKING_DIRECTORY ${HLSL_${target}_WORKDIRECTORY}
+            WORKING_DIRECTORY ${workDir}
+            VERBATIM)
+      endif()
+      # Compiled
+      if(NOT HLSL_${target}_OUTPUTDIRECTORY STREQUAL "" AND compileBatch)
+         add_custom_command(TARGET ${target}
+            COMMAND ${HLSL_SHADER_GLOBAL_COMPILER} /nologo /E main /T ${type}_${HLSL_${target}_SHADER_MODEL} $<IF:$<CONFIG:DEBUG>,/Od,/O${HLSL_SHADER_GLOBAL_OPTIMIZATION}> /Zi /Fo ${trueOutputDir}/${shaderRel}/${shaderFilename}.cso ${workDir}/${shaderRelFile}
+            MAIN_DEPENDENCY ${shader}
+            COMMENT "HLSL COMPILE ${shader}"
+            WORKING_DIRECTORY ${workDir}
             VERBATIM)
       endif()
    endforeach()
